e0992c4a250900adf6e9d4721f3a4b92bc774b07
luca <luca.defranceschi.91@gmail.com>
2015-02-22 14:04:19 +0100

    + edit AuthCtrl

--- app/scripts/controllers/AuthCtrl.js
+++ app/scripts/controllers/AuthCtrl.js
@@ -2,10 +2,31 @@
 
 angular.module('controllers')
 	
-	.controller('AuthCtrl', ['$scope', '$routeParams', '$location'
+	.controller('AuthCtrl', ['$scope', '$routeParams', '$location', 'ProfileService'
 
-		function($scope, $routeParams, $location) {
+		function($scope, $routeParams, $location, ProfileService) {
 
-			
+			$scope.userFormLogin = {
+				email: '',
+				password: ''
+			};
 
-		}]);
+			$scope.login = function() {
+
+				if ($scope.userFormLogin) {
+
+					var authUser = new ProfileService($scope.userFormLogin);
+					authUser.$login({},
+						function success(data) {
+							
+						}, function error(err) {
+							// Handle error
+						}
+					);
+
+				}
+
+			};
+
+		}
+	]);
